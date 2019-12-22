10d67cd42a54c28f065b69fe62945ca91c536b15
Jake Koperski <wijakoperski@gmail.com>
2019-12-22 14:42:14 -0600

    Added logic to EventLog to handle all types of events

--- PencilItIn/PencilItIn/Logic/EventLog.cs
+++ PencilItIn/PencilItIn/Logic/EventLog.cs
@@ -22,6 +22,48 @@ namespace PencilItIn.Logic
                         Payload = (CreateOfficeHoursEventPayload)payload
                     });
                     break;
+                case EventCode.AddBooking:
+                    this.Log.Push((IEvent<IEventPayload>)new AddBookingEvent()
+                    {
+                        Payload = (AddBookingEventPayload)payload
+                    });
+                    break;
+                case EventCode.CancelOfficeHours:
+                    this.Log.Push((IEvent<IEventPayload>)new CancelOfficeHoursEvent()
+                    {
+                        Payload = (CancelOfficeHoursEventPayload)payload
+                    });
+                    break;
+                case EventCode.ChangeEndTime:
+                    this.Log.Push((IEvent<IEventPayload>)new ChangeEndTimeEvent()
+                    {
+                        Payload = (ChangeEndTimeEventPayload)payload
+                    });
+                    break;
+                case EventCode.ChangeLocation:
+                    this.Log.Push((IEvent<IEventPayload>)new ChangeLocationEvent()
+                    {
+                        Payload = (ChangeLocationEventPayload)payload
+                    });
+                    break;
+                case EventCode.ChangeStartTime:
+                    this.Log.Push((IEvent<IEventPayload>)new ChangeStartTimeEvent()
+                    {
+                        Payload = (ChangeStartTimeEventPayload)payload
+                    });
+                    break;
+                case EventCode.ConfigureOfficeHours:
+                    this.Log.Push((IEvent<IEventPayload>)new ConfigureOfficeHoursEvent()
+                    {
+                        Payload = (ConfigureOfficeHoursEventPayload)payload
+                    });
+                    break;
+                case EventCode.RemoveBooking:
+                    this.Log.Push((IEvent<IEventPayload>)new RemoveBookingEvent()
+                    {
+                        Payload = (RemoveBookingEventPayload)payload
+                    });
+                    break;
                 default:
                     break;
             }
