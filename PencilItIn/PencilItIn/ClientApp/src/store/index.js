262a4be94b58ad900c3d39f82f758f8e4a4eff22
Jake Koperski <wijakoperski@gmail.com>
2020-01-07 23:42:47 -0600

    Removed default React app components

--- PencilItIn/PencilItIn/ClientApp/src/store/index.js
+++ PencilItIn/PencilItIn/ClientApp/src/store/index.js
@@ -1,14 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var WeatherForecasts = require("./WeatherForecasts");
-var Counter = require("./Counter");
 var OfficeHours = require("./OfficeHours");
 // Whenever an action is dispatched, Redux will update each top-level application state property using
 // the reducer with the matching name. It's important that the names match exactly, and that the reducer
 // acts on the corresponding ApplicationState property type.
 exports.reducers = {
-    officeHours: OfficeHours.reducer,
-    counter: Counter.reducer,
-    weatherForecasts: WeatherForecasts.reducer
+    officeHours: OfficeHours.reducer
 };
 //# sourceMappingURL=index.js.map
