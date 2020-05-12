686666a75c2ab8ab8d0f69ac75115e404876820b
Jake Koperski <wijakoperski@gmail.com>
2020-05-12 14:28:53 -0500

    Removed unused types

--- PencilItIn/PencilItIn/ClientApp/src/types.ts
+++ PencilItIn/PencilItIn/ClientApp/src/types.ts
@@ -1,17 +1,4 @@
-﻿/* Application Types */
-export type ApplicationState = {
-    officeHours: OfficeHoursState | undefined;
-};
-
-export type AppThunkAction<T> = (dispatch: (action: T) => void, getState: () => ApplicationState) => void;
-
-/* Office Hour Types */
-export type OfficeHoursState = {
-    officeHoursId: string,
-    isLoading: boolean
-};
-
-export type OfficeHours = {
+﻿export type OfficeHours = {
     id: string,
     startTime: Date,
     endTime: Date,
@@ -33,49 +20,6 @@ export type OfficeHoursResponseBody = {
     bookings: BookingResponseBody[]
 };
 
-export type OfficeHoursActionCreators = {
-    requestOfficeHours: (officeHoursId: string) => AppThunkAction<OfficeHoursAction>,
-    createBooking: (officeHoursId: string, booking: BookingRequestBody) => AppThunkAction<OfficeHoursAction>,
-    requestBooking: (officeHoursId: string, bookingId: string) => AppThunkAction<OfficeHoursAction>
-};
-
-export type RequestOfficeHoursAction = {
-    type: 'REQUEST_OFFICE_HOURS',
-    officeHoursId: string
-};
-
-export type ReceiveOfficeHoursAction = {
-    type: 'RECEIVE_OFFICE_HOURS',
-    officeHours: OfficeHours | undefined
-};
-
-export type CreateBookingAction = {
-    type: 'CREATE_BOOKING',
-    officeHoursId: string,
-    booking: BookingRequestBody
-};
-
-export type BookingCreatedAction = {
-    type: 'BOOKING_CREATED',
-    officeHoursId: string,
-    bookingId: string
-};
-
-export type RequestBookingAction = {
-    type: 'REQUEST_BOOKING',
-    officeHoursId: string,
-    bookingId: string
-};
-
-export type ReceiveBookingAction = {
-    type: 'RECEIVE_BOOKING',
-    officeHoursId: string,
-    booking: Booking
-};
-
-export type OfficeHoursAction = RequestOfficeHoursAction | ReceiveOfficeHoursAction | CreateBookingAction | BookingCreatedAction | RequestBookingAction | ReceiveBookingAction;
-
-/* Booking Types */
 export type Booking = {
     id: string,
     startTime: Date,
@@ -98,8 +42,6 @@ export type BookingRequestBody = {
     endTime: Date
 };
 
-/* COMPONENT TYPES */
-//export type OfficeHoursComponentProps = OfficeHoursState & OfficeHoursActionCreators & RouteComponentProps<{}>;
 export type OfficeHoursComponentProps = {
     officeHoursId: string
 };
